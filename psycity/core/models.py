c0c6c8b5a8f066112e96130fb3c33354c5be8084
Amin Masoudi <aminmasoudi2003@gmail.com>
2023-08-02 22:28:52 +0330

    ConstantConfig model added

--- psycity/core/models.py
+++ psycity/core/models.py
@@ -4,15 +4,11 @@ from django.utils.translation import gettext_lazy as _
 
 class Warehouse(models.Model):
     
-
     # question_id = models.ForeignKey("Question", on_delete=models.DO_NOTHING)
     # unlocker_id = models.ForeignKey("Player", on_delete=models.DO_NOTHING)
     # sensor_hacker = models.ForeignKey("Player", on_delete=models.DO_NOTHING)
     money = models.PositiveIntegerField()
-
     is_lock = models.BinaryField(default=True)
-
-
     sensor_state = models.BooleanField(defualt=False)
     expiration_date = models.DateTimeField(auto_now=False, auto_now_add=False)
     create_date = models.DateTimeField(auto_now=True, auto_now_add=True)  #FIXME 
@@ -38,3 +34,45 @@ class EscapeRoom(models.Model):
     bank_deposit_box = models.ForeignKey(BankDeposit, related_name="escape_rooms", on_delete=models.CASCADE)
     create_date = models.DateTimeField(auto_now=True, auto_now_add=True)  #FIXME 
     write_date = models.DateTimeField(auto_now=True, auto_now_add=True)
+
+
+class ConstantConfig(models.Model):
+    team_member_min = 2
+    team_member_max = 4
+    is_night = models.IntegerField(_("current_game_state"), choices=("DAY NIGHT"))
+    wallet_init_value = models.IntegerField()
+    question_level_0_value = models.IntegerField()
+    question_level_1_value = models.IntegerField()
+    question_level_2_value = models.IntegerField()
+    question_solve_interest_percent = models.PositiveIntegerField()
+    bought_question_max = models.PositiveIntegerField()
+    contract_tax = models.PositiveIntegerField()
+    inflation_coefficient = models.IntegerField()
+    delay_factor = models.FloatField()
+    question_level_0_max_try = 1
+    question_level_1_max_try = 2
+    question_level_2_max_try = 3
+    # question_code_max_try = 
+    question_level_0_early_solve_time = 40
+    question_level_1_early_solve_time = 80
+    question_level_2_early_solve_time = 150
+    deposit_interest_percent = models.PositiveIntegerField()
+    deposit_interest_day_time = models.PositiveIntegerField()
+    deposit_interest_night_time = models.PositiveIntegerField()
+    loan_interest_percent = models.PositiveIntegerField()
+    loan_interest_day_time = models.PositiveIntegerField()
+    loan_interest_night_time = models.PositiveIntegerField()
+    bonus_percent = models.PositiveIntegerField()    
+    penally_percent = models.PositiveIntegerField()
+    subsidy_percentage = models.PositiveIntegerField()
+    mafia_prison_per_report_time = models.PositiveIntegerField()
+    assassination_attempt_cooldown_time = models.PositiveIntegerField()
+    team_bank_transaction_cooldown = models.PositiveIntegerField()
+    team_total_bank_value = models.PositiveIntegerField()
+    team_loan_percent_max = models.PositiveIntegerField()
+    team_escape_room_max = 2
+    bank_robbery_contract_sponsorship_max = 2
+    police_sensor_contract_sponsorship_max = 5
+    escape_room_solve_time = models.PositiveIntegerField()
+
+
