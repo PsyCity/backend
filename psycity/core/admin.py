d44042f06b18fb56e49923bc9b85086830fefcc8
darloof <mrbayati2002@gmail.com>
2023-08-04 21:34:31 +0330

    [imp] register models in admin

--- psycity/core/admin.py
+++ psycity/core/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+admin.site.register(models.WarehouseBox)
+admin.site.register(models.BankDepositBox)
+admin.site.register(models.ConstantConfig)
+admin.site.register(models.Player)
+admin.site.register(models.Team)
+admin.site.register(models.TeamFeature)
+admin.site.register(models.TeamQuestionRel)
+admin.site.register(models.EscapeRoom)
+admin.site.register(models.Contract)
