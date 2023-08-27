de6780d4fbabc9fb87db17b7b1d4106f97d3e0de
At1X <amirmahdijafarian@gmail.com>
2023-08-27 20:35:32 +0330

    add leave & join team logics

--- psycity/player_api/views.py
+++ psycity/player_api/views.py
@@ -1,3 +1,69 @@
-from django.shortcuts import render
+from django.db import transaction
+from rest_framework.response import Response
+from rest_framework.generics import UpdateAPIView
+from rest_framework import status
+from core.models import TeamJoinRequest
+from core.models import Player
 
-# Create your views here.
+class PlayerLeftTeam(UpdateAPIView):
+    def patch(self, request, *args, **kwargs):
+        try:
+            player = Player.objects.get(pk=request.data["player_id"])
+        except Player.DoesNotExist:
+            return Response({
+                "message": "player doesn't exist",
+                "data": [],
+                "result": None,
+            }, status=status.HTTP_404_NOT_FOUND)
+        if player.team is not None:
+            player.team = None
+            player.save()
+            return Response({
+                "message": "player team removed",
+                "data": [],
+                "result": None,
+            }, status=status.HTTP_200_OK) 
+        else:
+            return Response({
+                "message": "player doesn't have any team",
+                "data": [],
+                "result": None,
+            }, status=status.HTTP_400_BAD_REQUEST)   
+
+class PlayerJoinTeam(UpdateAPIView):
+    @transaction.atomic
+    def patch(self, request):
+        try:
+            found_request = TeamJoinRequest.objects.get(pk=request.data["request_id"])
+            if (found_request.state == "inactive"):
+                return Response({
+                    "message": "team join request is already inactive",
+                    "data": [],
+                    "result": None,
+                }, status=status.HTTP_400_BAD_REQUEST)
+            found_request.player.team = found_request.team
+            found_request.player.save()
+            TeamJoinRequest.objects.filter(player=found_request.player).update(state="inactive")
+            return Response({
+                "message": "player joined the team",
+                "data": [],
+                "result": None,
+            }, status=status.HTTP_200_OK)
+        except TeamJoinRequest.DoesNotExist:
+            return Response({
+                "message": "team join request doesn't exist",
+                "data": [],
+                "result": None,
+            }, status=status.HTTP_404_NOT_FOUND)
+        except:
+            return Response({
+                "message": "something went wrong",
+                "data": [],
+                "result": None,
+            }, status=status.HTTP_400_BAD_REQUEST)
+        
+        
+        
+
+    
+    
