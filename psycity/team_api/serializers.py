7dc9c67e3d5916cd02a694824f4596c861d30552
Amin Masoudi <aminmasoudi2003@gmail.com>
2023-09-06 23:12:09 +0330

    FIX => two validator added

--- psycity/team_api/serializers.py
+++ psycity/team_api/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from rest_framework.exceptions import NotAcceptable, APIException
 from core.models import Team, Player, PlayerRole, TeamJoinRequest
 from rest_framework.exceptions import bad_request
 class TeamSerializer(serializers.ModelSerializer):
@@ -19,7 +20,17 @@ class TeamMemberSerializers(serializers.Serializer):
     agreement   = serializers.ListField(required=True, write_only=True)
 
 class TeamJoinRequestSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = TeamJoinRequest
         fields = ["player", "team"]
 
+    def validate_team(self, team):
+        if (team.player_team.count() > 3):
+           raise  NotAcceptable("team is full", 406)
+        return team
+    
+    def validate_player(self, player):
+        if player.team_id:
+            raise NotAcceptable("player is not homeless")
+        return player
