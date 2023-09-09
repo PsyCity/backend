79bf2f13ef16c99880d03e1e2b17eb9de1532ad8
Amin Masoudi <aminmasoudi2003@gmail.com>
2023-09-09 14:28:03 +0330

    FIX => agreement

--- psycity/team_api/schema.py
+++ psycity/team_api/schema.py
@@ -7,19 +7,7 @@ from . import serializers
 
 agreement_schema = \
     openapi.Schema(
-        type=openapi.TYPE_ARRAY,
-        items=openapi.Schema(
-            type=openapi.TYPE_OBJECT,
-            properties={
-                "player_id" : openapi.Schema(
-                    type=openapi.TYPE_INTEGER,
-                ),
-                "is_agree" : openapi.Schema(
-                    type=openapi.TYPE_BOOLEAN
-                )
-            }
-            
-            )
+        type=openapi.TYPE_INTEGER,
         )
 
 
