d70208758c491bd832afa2108d8a2aa6d01477f3
Amin Masoudi <aminmasoudi2003@gmail.com>
2023-09-26 17:41:00 +0330

    FIX => some litle bugs

--- psycity/models_retrieve_api/serializers.py
+++ psycity/models_retrieve_api/serializers.py
@@ -1,19 +1,25 @@
 from rest_framework.serializers import ModelSerializer
-
-from core.models import Question
-class TeamListSerializer:
+from rest_framework import serializers
+from core.models import (
+    Team,
+    Question
+)
+class TeamListSerializer(ModelSerializer):
     class Meta:
+        model = Team
         fields = [
             "id",
             "name",
             "state"
         ]
 
-class TeamRetrieveSerializer:
+class TeamRetrieveSerializer(ModelSerializer):
     class Meta:
+        model = Team
         fields = "__all__"
 
-
+    def get_players(self, obj):
+        return obj.name
 
 class QuestionListSerializer(ModelSerializer):
     class Meta:
