38dcb1e1a35a7f1e73b85eeae604ce30182c9c7e
Amin Masoudi <aminmasoudi2003@gmail.com>
2023-09-27 18:47:57 +0330

    IMP => contracts list-retrieve endpoint

--- psycity/models_retrieve_api/urls.py
+++ psycity/models_retrieve_api/urls.py
@@ -8,8 +8,16 @@ from . import views
 app_name = "models_retrieve_api"
 
 router = DefaultRouter()
-router.register("teams", views.TeamViewSet)
-router.register("questions", views.QuestionViewSet, "questions")
+router.register("teams",
+                views.TeamViewSet
+                )
+router.register(prefix="questions",
+                viewset=views.QuestionViewSet,
+                basename="questions"
+                )
+router.register(prefix="contracts",
+                viewset=views.ContractViewSet
+                )
 
 urlpatterns = [
     path("", include(router.urls))
