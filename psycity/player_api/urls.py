21044ca4b8df98655b401b9953c720009e1a5b9d
AminMasoudi <aminmasoudi2003@gmail.com>
2023-10-02 22:39:05 +0330

    FIX => urls

--- psycity/player_api/urls.py
+++ psycity/player_api/urls.py
@@ -1,16 +1,10 @@
-from django.urls import path, include
+from django.urls import path
 from . import views
-from rest_framework.routers import DefaultRouter
 
-loan_router = DefaultRouter()
-loan_router.register(prefix="receive", 
-                     viewset=views.LoanReceive,
-                     basename="loan"
-                     )
 
 urlpatterns = [
     path('team/leave/', views.PlayerLeftTeam.as_view()),
     path('team/join/', views.PlayerJoinTeam.as_view()),
     path("id_by_discord/", views.PlayerIdByDiscord.as_view(), name="discord_player_id"),
-    path("loan/", include(loan_router.urls)),
+    path("loan/receive", views.LoanReceive.as_view(), name="loan-receive"),
 ]
