0c7b3c38bab156f2ea235bbe1bf08ee639cf1948
darloof <mrbayati2002@gmail.com>
2023-10-06 18:36:59 +0330

    [imp][fix] file store route to get question body preview in adminpanel

--- psycity/core/views.py
+++ psycity/core/views.py
@@ -1,3 +1,13 @@
+import os
 from django.shortcuts import render
+from django.http import HttpResponse
+from psycity.settings import BASE_DIR
 
-# Create your views here.
+def data_dir_api(request, filedir, filename):
+    print(os.path.join(BASE_DIR, 'data_dir', filedir, filename), 'r')
+    try:
+        with open(os.path.join(BASE_DIR, 'data_dir', filedir, filename), 'rb') as f:
+            return HttpResponse(f.read(), content_type="image/jpeg")
+    except Exception as e:
+        raise e
+        return HttpResponse('<h1>File Not Found!</h1>', status=404)
