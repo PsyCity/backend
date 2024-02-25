e00fe894fa27144d451cf575bd2d28854fa61264
darloof <mrbayati2002@gmail.com>
2024-02-25 21:40:54 +0330

    [fix] player discord_id field type changed to integer

--- psycity/core/models.py
+++ psycity/core/models.py
@@ -182,7 +182,7 @@ class Player(BaseModel):
                                           on_delete=models.DO_NOTHING,
                                           related_name='player_bodyguard_team')
     last_bodyguard_cost = models.IntegerField(default=0)
-    discord_id = models.CharField(max_length=100, null=False, blank=False, unique=True, primary_key=True)
+    discord_id = models.IntegerField(null=False, blank=False, unique=True, primary_key=True)
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
