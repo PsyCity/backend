fcc2001742951187f2a13affd7575067ce9dc8fe
Amin Masoudi <aminmasoudi2003@gmail.com>
2024-03-04 13:14:17 +0330

    UPD =>. use hidden_id

--- psycity/report/views.py
+++ psycity/report/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from rest_framework.viewsets import GenericViewSet, mixins
 from rest_framework.response import Response
 from rest_framework import status
-from core.models import Report
+from core.models import Report, Team
 from team_api.utils import response
 from .serializers import SimpleReportSerializer, ContractReportSerializer
 # Create your views here.
@@ -40,9 +40,23 @@ class ContractReportViewSet(
     
     serializer_class = ContractReportSerializer
     queryset = Report.objects.all()
-
+    team_query_set = Team.objects.all()
+ 
     @response
     def create(self, request, *args, **kwargs):
+        if request.data.get('team_reporter'):
+            if len(str(request.data.get('team_reporter'))) == 12:
+                team = self.team_query_set.filter(hidden_id=request.data.get('team_reporter'))
+                if not team:
+                    return Response(
+                     data={
+                        "message": "team_reporter not found.",
+                        "data": [],
+                        "result": None,
+                        },
+                        status=status.HTTP_404_NOT_FOUND,
+                    )
+                request.data['team_reporter'] = team.first().channel_role
 
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
