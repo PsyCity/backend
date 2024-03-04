f8e2e694c3aaafca166dd4995260904ae342486e
Amin Masoudi <aminmasoudi2003@gmail.com>
2024-03-04 10:38:33 +0330

    IMP => contract report serializer

--- psycity/report/serializers.py
+++ psycity/report/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
-from core.models import Report
-
+from core.models import Report, Contract, Team
+from rest_framework import exceptions
 
 class SimpleReportSerializer(
     serializers.ModelSerializer
@@ -12,5 +12,40 @@ class SimpleReportSerializer(
             "description"
         )
 
-    def create(self, validated_data):
-        return super().create(validated_data)
+
+class ContractReportSerializer(
+    serializers.ModelSerializer
+    ):
+
+    class Meta:
+        model = Report
+        fields = (
+            "contract",
+            "description",
+            "team_reporter"
+        )
+
+    def validate_contract(self, obj: Contract):
+        if not (
+            (obj.state == 2) and\
+            obj.first_party_agree and\
+            obj.second_party_agree 
+            ):
+            raise exceptions.ValidationError(
+                "obj is not in a correct state or not signed by both side."
+            )
+        return obj
+    
+    def is_valid(self, *, raise_exception=False):
+        return super().is_valid(raise_exception=raise_exception)
+    
+    def is_acceptable(self):
+        contract :Contract = self.validated_data["contract"]
+        reporter : Team = self.validated_data["team_reporter"]
+        l = []
+        if contract.first_party_team: l.append(contract.first_party_team.pk)
+        if contract.second_party_team: l.append(contract.second_party_team.pk)
+        if reporter.pk not in l:
+            raise exceptions.NotAcceptable(
+                "the team is not in the contract."
+            )
