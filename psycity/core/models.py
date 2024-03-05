ad7e1be09cdaed2724931abd45ce2b0e48402188
darloof <mrbayati2002@gmail.com>
2024-03-05 09:39:50 +0330

    [fix] filefields PathAndRename

--- psycity/core/models.py
+++ psycity/core/models.py
@@ -270,8 +270,8 @@ class Question(BaseModel):
     body = models.ImageField(upload_to=PathAndRename('data_dir/question'))
     qtype = models.IntegerField(choices=TYPE_CHOICE)
     answer_text = models.TextField(blank=True, null=True)
-    answer_file = models.FileField(blank=True, null=True, upload_to='question_answer_file')
-    attachment = models.FileField(blank=True, null=True, upload_to='question_attachment')
+    answer_file = models.FileField(blank=True, null=True, upload_to=PathAndRename('data_dir/question_answer_file'))
+    attachment = models.FileField(blank=True, null=True, upload_to=PathAndRename('data_dir/question_attachment'))
 
     def body_preview(self): #new
         return mark_safe(f'<img src = "{self.body.url}" width = "300"/>')
@@ -294,7 +294,7 @@ class QuesionSolveTries(BaseModel):
     solved = models.BooleanField(default=False)
     received_score = models.IntegerField(default=False)
     answer_text = models.TextField(null=True, blank=True)
-    answer_file = models.FileField(blank=True, null=True, upload_to='questionsolvetries_answer_file')
+    answer_file = models.FileField(blank=True, null=True, upload_to=PathAndRename('data_dir/questionsolvetries_answer_file'))
 
 class EscapeRoom(BaseModel):
     ESCAPE_ROOM_STATE = [
