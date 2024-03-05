1d8c21731691ec6581065547091be12805f49099
Amin Masoudi <aminmasoudi2003@gmail.com>
2024-03-05 11:09:27 +0330

    UPD => filter contracts in list

--- psycity/team_api/views/contract.py
+++ psycity/team_api/views/contract.py
@@ -276,5 +276,6 @@ class Reject(generics.UpdateAPIView):
 class TeamContracts(generics.GenericAPIView):
     def get(self, request, team_id, *args, **kwargs):
         contracts = Contract.objects.filter(first_party_team=team_id) | Contract.objects.filter(second_party_team=team_id)
+        contracts = list(filter(lambda contract: contract.state in [2], contracts))
         serializer = TeamContractListSerializer(contracts, many=True)
         return Response(serializer.data)
