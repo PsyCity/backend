80942b6055edc34fd28cedc04dfbb9a73c51e7bb
Amin Masoudi <aminmasoudi2003@gmail.com>
2024-03-08 13:35:30 +0330

    isdfn

--- psycity/core/views.py
+++ psycity/core/views.py
@@ -2,7 +2,8 @@ import os
 from django.shortcuts import render
 from django.http import HttpResponse
 from psycity.settings import BASE_DIR
-
+from django.views.generic import TemplateView, ListView
+from core.models import Team
 def data_dir_api(request, filedir, filename):
     print(os.path.join(BASE_DIR, 'data_dir', filedir, filename), 'r')
     try:
@@ -10,3 +11,16 @@ def data_dir_api(request, filedir, filename):
             return HttpResponse(f.read(), content_type="image/jpeg")
     except Exception as e:
         return HttpResponse('<h1>File Not Found!</h1>', status=404)
+
+
+class LeaderView(TemplateView):
+    template_name = "core/leader.html"
+
+    extra_context = {"teams": list(Team.objects.all()).sort(key=lambda x: x.total_asset)}
+    
+
+def leaderboard(request):
+    teams = list(Team.objects.all())
+
+    teams.sort(reverse=True, key=lambda x: x.total_asset)        
+    return render(request, 'core/leader.html', {"teams": teams})
